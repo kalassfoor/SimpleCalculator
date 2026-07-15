f50fc08573a63a939702f9d3ebf543d9c5d23139
kalassfoor <49289310+kalassfoor@users.noreply.github.com>
2026-07-15 11:57:41 +0300

    Update test.java

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test {
 
     public static void main(String[] args) {
 
-        if (calculator.add(2, 2) != 4) {
+        if (calculator.add(2, 2) != 5) {
             throw new RuntimeException("Test Failed");
         }
 
